ad19fdb0688809b69e45b98763707bd6fd763fed
xhrisgab <xhris.gab@gmail.com>
2025-03-08 12:17:50 -0400

    Pantalla control agregado

--- src/app/pages/control/control.component.ts
+++ src/app/pages/control/control.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LoraService } from '../../services/lora.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-control',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css'
 })
 export class ControlComponent {
+
+  public loraService = inject(LoraService);
+  //const cp = new CircleProgress();
 
 }
---
+++ src/app/services/lora.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class LoraService {
+    constructor() { }
+
+    loraDate = new Date();
+    
+}
