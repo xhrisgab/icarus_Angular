bdc5f0df8a1452d053e91ee05d705d2a01e9242a
xhrisgab <xhris.gab@gmail.com>
2025-05-14 15:03:30 -0400

    regreso enviroment a icarusapi.serveo.net

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    //backUrl: "https://icarusapi.serveo.net"
-    backUrl: "https://164e-177-222-59-170.ngrok-free.app"
+    backUrl: "https://icarusapi.serveo.net"
+    //backUrl: "https://164e-177-222-59-170.ngrok-free.app"
 }
